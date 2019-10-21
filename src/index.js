6b988cf414a2c0a1e2b579d759160c1b9da44573
Oleksandr Zhuravlov <rejifald@gmail.com>
2019-10-21 03:30:02 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const { Storage } = require('@google-cloud/storage')
+
+module.exports = {
+  provider: 'gcs',
+  name: 'Google Cloud Storage',
+  auth: {
+    account: {
+      label: 'Service Account JSON',
+      type: 'textarea'
+    },
+    bucket: {
+      label: 'Bucket name',
+      type: 'text'
+    }
+  },
+  init: ({ bucket, account }) => {
+    const credentials = JSON.parse(account)
+
+    const storage = new Storage({
+      projectId: credentials.project_id,
+      credentials: {
+        client_email: credentials.client_email,
+        private_key: credentials.private_key
+      }
+    })
+      .bucket(bucket)
+
+    return {
+      upload: async (file) => {
+        const bucketFile = storage.file(file.hash + file.ext.toLowerCase())
+
+        try {
+          await bucketFile.save(Buffer.from(file.buffer), {
+            contentType: file.mime
+          })
+        } catch (e) {
+          throw (e instanceof Error ? e : new Error(e))
+        }
+
+        file.url = `https://storage.cloud.google.com/${bucket}/${bucketFile.name}`
+      },
+      delete: file => {
+        throw new Error('File deletion not implemented yet')
+      }
+    }
+  }
+}
